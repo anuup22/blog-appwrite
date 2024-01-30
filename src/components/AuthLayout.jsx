80c4f14c21b51e9caf46c98d30af7b45c5c83ee7
Anup <anuup22@outlook.com>
2024-01-30 08:58:54 +0530

    react-hook-form

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+export default function Protected({ children, authentication = true }) { 
+    const navigate = useNavigate();
+    const [loader, setLoader] = useState(true);
+    const authStatus = useSelector((state) => state.auth.status);
+
+    useEffect(() => {
+        if(authentication && authStatus !== authentication){
+            navigate("/login")
+        } else if(!authentication && authStatus !== authentication){
+            navigate("/")
+        }
+        setLoader(false)
+    } , [authStatus, navigate, authentication]);
+
+    return (
+        <div className="flex items-center justify-center">
+            <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+                <div className="mb-2 flex justify-center">
+                    <span className="inline-block w-full max-w-[100px]">
+                        <Logo width="100%" />
+                    </span>
+                </div>
+                {children}
+            </div>
+        </div>
+    );
+}
